8c5be3013d4906adcd56a0120e630e95406d0a5b
jjagn <72364545+jjagn@users.noreply.github.com>
2021-01-31 13:58:12 +1300

    intial setup of open and close commands

--- hello-flask.py
+++ hello-flask.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from gpiozero import Motor
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "twelve of the clock and all is well"
+
+@app.route("/open")
+def open():
+    motor.forward()
+    sleep(5)
+    motor.stop()
+
+@app.route("/close")
+def close():
+    motor.backward()
+    sleep(5)
+    motor.stop()
